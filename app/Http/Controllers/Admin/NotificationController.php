27a1a518d891b1e1783f42211aa4c924d921e32f
wanghuy <huytqph52907@gmail.com>
2025-11-18 17:16:18 +0700

    chore: push workspace to wanghuy (user requested)

--- app/Http/Controllers/Admin/NotificationController.php
+++ app/Http/Controllers/Admin/NotificationController.php
@@ -1,31 +1,31 @@
 <?php
 
-namespace App\Http\Controllers\Admin;
+// namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Auth;
+// use App\Http\Controllers\Controller;
+// use Illuminate\Http\Request;
+// use Illuminate\Support\Facades\Auth;
 
-class NotificationController extends Controller
-{
-    public function markAsRead($id)
-    {
-        $user = Auth::user();
-        if (! $user) return redirect()->back();
+// class NotificationController extends Controller
+// {
+//     public function markAsRead($id)
+//     {
+//         $user = Auth::user();
+//         if (! $user) return redirect()->back();
 
-        $notif = $user->notifications()->where('id', $id)->first();
-        if ($notif) {
-            $notif->markAsRead();
-            $data = $notif->data ?? [];
-            if (isset($data['order_id'])) {
-                // Redirect to admin order show if possible
-                try {
-                    return redirect()->route('orders.show', $data['order_id']);
-                } catch (\Throwable $_) {
-                    return redirect()->back();
-                }
-            }
-        }
-        return redirect()->back();
-    }
-}
+//         $notif = $user->notifications()->where('id', $id)->first();
+//         if ($notif) {
+//             $notif->markAsRead();
+//             $data = $notif->data ?? [];
+//             if (isset($data['order_id'])) {
+//                 // Redirect to admin order show if possible
+//                 try {
+//                     return redirect()->route('orders.show', $data['order_id']);
+//                 } catch (\Throwable $_) {
+//                     return redirect()->back();
+//                 }
+//             }
+//         }
+//         return redirect()->back();
+//     }
+// }
